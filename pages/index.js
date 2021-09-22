39fa00887e338253ce1229d8d28b88202e7d0158
Mohamed Ashraf <mashrafelsaeed@gmail.com>
2021-09-22 15:26:19 +0200

    updated versioin

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react';
-import styles from '../styles/Home.module.css'
 import { ApolloClient, InMemoryCache, gql, useQuery, ApolloProvider } from "@apollo/client";
 import { HeaderComponent } from '../comps/HeaderComponent';
 import { StyledDiv } from '../comps/StyledDiv'
-import { ValuesOfCorrectTypeRule } from 'graphql';
-import useSWR, { SWRConfig } from 'swr'
+import useSWR from 'swr'
 import { request } from 'graphql-request'
-import { valueToObjectRepresentation } from '@apollo/client/utilities';
-
-
-const fetcher = query => request('https://graphql-sfv4.zyda.com/graphql/', QUERY_STRING)
+const fetcher = () => request('https://graphql-sfv4.zyda.com/graphql/', QUERY_STRING)
 const QUERY_STRING = gql`
 query{
   store(subdomain: "demo-sfv4") {
@@ -27,8 +18,7 @@ query{
       maxOrderFulfillmentPeriod
       pickupEnabled
       pickupOrdersType
-      restaurantId
-    
+      restaurantId 
     }   
     branchesCount
     descriptionAr
@@ -41,19 +31,12 @@ query{
   }
 }
 `;
-
-
-
 export default function Home() {
   const { data: values, error: errors } = useSWR(QUERY_STRING, fetcher)
   const { loading, error, data } = useQuery(QUERY_STRING);
   if (loading) return <p>Loading....</p>;
   if (error) return <p>Error :(</p>;
   const { carPickup, delivery, pickup } = values.store.enabledOrderModes
-
-
-  console.log(values.store.descriptionEn)
-
   return (
     <div>
       <HeaderComponent >{values.store.descriptionEn}</HeaderComponent>
@@ -73,62 +56,15 @@ export default function Home() {
           <p>Pickup Order Type : {branch.pickupOrdersType}</p>
           <p>Restaurant ID : {branch.restaurantId}</p>
         </StyledDiv>
-
-
       }
-
       )}
-
-
-
-
     </div>
-
-
   )
 }
 const client = new ApolloClient({
   uri: 'https://graphql-sfv4.zyda.com/graphql',
   cache: new InMemoryCache()
 });
-
-
-
-
 client.query({
-  query: gql`query{
-    store(subdomain: "demo-sfv4") {
-      branches{
-        areaEn
-        contactNumber
-        createdAt
-        delivery
-        deliveryOrdersEnabled
-        id
-        dineIn
-        maxOrderFulfillmentPeriod
-        pickupEnabled
-        pickupOrdersType
-        restaurantId
-        phoneNumber
-        
-      }
-      
-      branchesCount
-      descriptionEn
-  
-      enabledOrderModes{
-        carPickup
-        delivery
-        pickup
-      }
-      
-      
-      
-    }
-  }
-  
-    
-    `
-}).then(result => console.log(result + "ss"))
-
+  query: QUERY_STRING
+}).then(result => console.log(result))
